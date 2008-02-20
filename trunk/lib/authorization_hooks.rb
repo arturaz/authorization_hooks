d578ed07ebb6abe76e01ef7ef4d7a98b3ddee200
x11 <x11@a439b4bb-83d8-46c6-86ec-f6d4d22dddae>
2008-02-20 01:58:17 +0000

    Added recover_password and new_password urls to default excludes.
    git-svn-id: svn://arturaz.net/public/rails/plugins/authorization_hooks@425 a439b4bb-83d8-46c6-86ec-f6d4d22dddae

--- trunk/lib/authorization_hooks.rb
+++ trunk/lib/authorization_hooks.rb
@@ -6,5 +6,6 @@ module AuthorizationHooks
   # Message that is shown in flash[:error] when session gets corrupted.  
   self.reset_user_session_message = "Please relogin."
   # Do not save last location in these routes.
-  self.do_not_save_location_in = [:login, :logout, :new_user]
+  self.do_not_save_location_in = [:login, :logout, :new_user, 
+    :recover_password, :new_password]
 end
