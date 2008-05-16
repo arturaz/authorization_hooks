cb5e48ba72a019083d62df9366d9a3776e9c8e44
x11 <x11@a439b4bb-83d8-46c6-86ec-f6d4d22dddae>
2008-05-16 20:44:06 +0000

    Added post saving.
    git-svn-id: svn://arturaz.net/public/rails/plugins/authorization_hooks@790 a439b4bb-83d8-46c6-86ec-f6d4d22dddae

--- trunk/lib/arturaz/authorization_hooks.rb
+++ trunk/lib/arturaz/authorization_hooks.rb
@@ -17,6 +17,12 @@ module Arturaz
       def check_authorization
         unless authorized?
           save_last_location
+          if request.post?
+            save_params
+            session[:post_last_location] = session[:last_location]
+            session[:last_referer] = request.env["HTTP_REFERER"]
+            session[:last_location] = post_after_authorization_url
+          end
           flash[:notice] ||= ::AuthorizationHooks.flash_notice
           begin
             redirect_to login_url
@@ -43,6 +49,11 @@ module Arturaz
         session[:last_location] = request.env["REQUEST_URI"]
         true
       end
+      
+      def save_params
+	session[:params] = params.except(:controller, :action, 
+          :authenticity_token).to_hash
+      end
 
       # Renew session expiration date after each request. So that keep me logged in 
       # for 2 weeks will always be 2 weeks from last request.
@@ -57,6 +68,32 @@ module Arturaz
       def authorized?
         controller.send :authorized?
       end
+      
+      # Creates a form that is used to redirect post data automatically
+      def redirection_form
+        # Get redirection URL
+        redirection_url = session[:post_last_location] || root_url
+        session[:post_last_location] = nil
+
+        # Get redirection params
+        redirection_params = session[:params] || {}
+        session[:params] = nil
+
+        # Restore referer
+        if session[:last_referer]
+          session[:last_location] = session[:last_referer] 
+          session[:last_referer] = nil
+        end    
+
+        form_id = "redirection_form"
+        html = form_tag(redirection_url, :id => form_id) + "\n"
+        redirection_params.formify.each do |k, v|
+          html += hidden_field_tag(k, v) + "\n"
+        end
+        html + submit_tag(::AuthorizationHooks.redirection_submit_label) + 
+          "</form>" + javascript_tag("document.getElementById(" +
+            form_id.to_json + ").submit()")
+      end
     end
   end
 end
@@ -69,7 +106,7 @@ ActionController::Base.class_eval do
   
   after_filter :save_last_location, :renew_session
   protected :reset_user_session, :authorized?, :check_authorization,
-    :save_last_location, :renew_session
+    :save_last_location, :save_params, :renew_session
 end
 
 ActionView::Base.send :include, Arturaz::AuthorizationHooks::HelperMethods
